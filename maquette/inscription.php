2e277ef9671e852e32e83cd95bf13bdd8064b7f1
MatheoMoreira <matheo.verite42@gmail.com>
2024-10-30 13:39:06 +0000

    commit

--- maquette/inscription.php
+++ maquette/inscription.php
@@ -9,66 +9,54 @@ ob_start();
 
 include("template/header.html");
 ?>
-<style>
-    .registration-form {
-        width: 100%;
-        max-width: 300px;
-        padding: 20px;
-        background-color: #8c8c8d;
-        border-radius: 10px;
-        box-shadow: 0 0 10px rgba(32, 3, 251, 0.1);
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        align-items: center;
-    }
-</style>
-<body class="d-flex justify-content-center align-items-center vh-100 bg-light">
-<div class="registration-form">
-    <h2 class="text-center mb-4">Inscription</h2>
-    <form action="submit_registration.php" method="post">
-        <div class="row mb-3">
-            <div class="col-6">
-                <label for="firstname">Prénom :</label>
-                <input type="text" class="form-control" id="firstname" name="firstname" required>
-            </div>
-        <!-- </div>
-        <div class="row mb-3"> -->
-            <div class="col-6">
-                <label for="lastname">Nom :</label>
-                <input type="text" class="form-control" id="lastname" name="lastname" required>
-            </div>
+<div class="d-flex justify-content-center align-items-center vh-100">
+    <div class="form">
+        <div class="d-flex justify-content-center align-items-center mb-4">
+            <img src="assets/images/favicon.ico" style="height:40px; width: 40px; margin-right: 10px;" class="card-img-top" alt="Logo Navi">  
+            <h2 class="card-title">Inscription</h5>
         </div>
-        <div class="row mb-3">
-            <div class="col-12">
-                <label for="email">Email :</label>
-                <input type="email" class="form-control" id="email" name="email" required>
+        <form action="submit_registration.php" method="post">
+            <div class="row mb-3">
+                <div class="col-6">
+                    <label for="firstname">Prénom :</label>
+                    <input type="text" class="form-control" id="firstname" name="firstname" required>
+                </div>
+                <div class="col-6">
+                    <label for="lastname">Nom :</label>
+                    <input type="text" class="form-control" id="lastname" name="lastname" required>
+                </div>
             </div>
-        </div>
-        <div class="row mb-3">
-            <div class="col-12">
-                <label for="password">Mot de passe :</label>
-                <input type="password" class="form-control" id="password" name="password" required>
+            <div class="row mb-3">
+                <div class="col-12">
+                    <label for="email">Email :</label>
+                    <input type="email" class="form-control" id="email" name="email" required>
+                </div>
             </div>
-        </div>
-        <div class="row mb-3">
-            <div class="col-12">
-                <label for="confirm_mdp">Confirmation de mot de passe :</label>
-                <input type="password" class="form-control" id="confirm_mdp" name="confirm_mdp" required>
+            <div class="row mb-3">
+                <div class="col-12">
+                    <label for="password">Mot de passe :</label>
+                    <input type="password" class="form-control" id="password" name="password" required>
+                </div>
             </div>
-        </div>
-        <div class="row mb-3">
-            <div class="col-12">
-                <label for="class">Classe / section :</label>
-                <input type="text" class="form-control" id="class" name="class" required>
+            <div class="row mb-3">
+                <div class="col-12">
+                    <label for="confirm_mdp">Confirmation de mot de passe :</label>
+                    <input type="password" class="form-control" id="confirm_mdp" name="confirm_mdp" required>
+                </div>
             </div>
-        </div>
-        <div class="row">
-            <div class="col-12 d-grid">
-                <button type="submit" class="btn btn-primary">S'inscrire</button>
+            <div class="row mb-3">
+                <div class="col-12">
+                    <label for="class">Classe / section :</label>
+                    <input type="text" class="form-control" id="class" name="class" required>
+                </div>
             </div>
-        </div>
-    </form>
+            <div class="row">
+                <div class="col-12 d-grid">
+                    <button type="submit" class="btn btn-primary">S'inscrire</button>
+                </div>
+            </div>
+        </form>
+    </div>
 </div>
 
 <?php
