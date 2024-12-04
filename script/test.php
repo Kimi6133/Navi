98a0259d44d09a4abb93cc9bc07d3a713832a707
fabrice1618 <fabrice1618@gmail.com>
2024-12-04 04:44:34 +0100

    script base

--- script/test.php
+++ script/test.php
@@ -2,12 +2,12 @@
 // Mesure du temps d'execution
 $index_start = microtime(true);
 
-if ( !isset($_SERVER['DOCUMENT_ROOT'])) {
-    throw new \Exception("Fatal error: This application must be run in a web environnement.", 1);
+if (! empty($_SERVER['DOCUMENT_ROOT'])) {
+    throw new \Exception("Fatal error: This application must be run in a CLI environnement.", 1);
 }
 
 // Configuration dossiers
-$sBasepath = $_SERVER['DOCUMENT_ROOT'].'/';
+$sBasepath = '/script/';
 echo "Dossier base: {$sBasepath}\n";
 $sClassPath = $sBasepath . 'src/';
 require_once($sClassPath . "autoload.php");
@@ -16,6 +16,6 @@ require_once($sClassPath . "autoload.php");
 // Calcul du temps d'exécution
 $index_end = microtime(true);
 $temps_execution = ($index_end - $index_start) * 1000;
-echo "Le temps d'exécution de la page est de : " . number_format($temps_execution, 3, '.', '') . " ms";
+echo "Le temps d'exécution de la page est de : " . number_format($temps_execution, 3, '.', '') . " ms\n";
 
 
